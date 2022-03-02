ddf719215bfd23a97878017baab2b38d6256a91b
Ingo Ruhnke <grumbel@gmail.com>
2022-03-02 17:56:04 +0100

    Remove superfluous nix input from Nix flake

--- flake.nix
+++ flake.nix
@@ -2,12 +2,11 @@
   description = "Generation of random strings from the command line";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
-    nix.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nix, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -18,8 +17,6 @@
             src = self;
             propagatedBuildInputs = [
               pkgs.python3Packages.setuptools
-              pkgs.python3Packages.pyqt5
-              pkgs.python3Packages.pyxdg
             ];
           };
         };
