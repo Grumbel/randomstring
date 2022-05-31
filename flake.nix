0c94e320c768682e8fe9807e55f5a1fe9f72c6ff
Ingo Ruhnke <grumbel@gmail.com>
2022-05-31 19:45:20 +0200

    Update flake.nix to nixos-22.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Generation of random strings from the command line";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
